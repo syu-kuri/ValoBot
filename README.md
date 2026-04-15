578d3cc71a3e125fdabaf27970a658902adce32e
syu-kuri <72872356+syu-kuri@users.noreply.github.com>
2026-04-16 08:26:23 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Valorant Custom Bot
+This project is a bot for managing Valorant custom games on Discord.
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from Valrant Custom Bot!")
+
+
+if __name__ == "__main__":
+    main()
